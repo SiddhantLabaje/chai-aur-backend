b5084296622907c077619c01c88b26c3e06492b0
SiddhantLabaje <labajesiddhant07@gmail.com>
2026-08-01 22:47:44 +0530

    all backend end aggregation pipeline remaining

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -369,6 +369,71 @@ const updateCoverImage=asyncHandler(async(req,res)=>{
 })
 
 
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {username}=req.params
+    if(!username?.trim()){
+        throw new ApiError(400,"username is required")
+    }
+   const channel = await User.aggregate([
+    {
+    $match:
+    {
+        username:username?.toLowerCase()
+    }
+},
+    {
+    $lookup:{
+        from:"Subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+    }
+    },
+    {
+        $lookup:{
+        from:"Subscriptions",
+        localField:"_id",
+        foreignField:"Subscriber",
+        as:"subscribedTo"
+    }
+    },
+    {
+        $addFields:{
+            subscribersCount:{$size:"$subscribers"},
+            subscribedToCount:{$size:"$subscribedTo"},
+            isSubscribed:{
+                $cond:{
+                    if:{$in:[req.user?._id,"$subscribers.Subscriber"]},
+                    then:true,
+                    else:false
+                }
+            }
+        }
+    },
+    {
+        $project:{
+            fullName:1,
+            username:1,
+            subscribersCount:1,
+            subscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1,
+        }
+    }
+
+])
+    if(!channel?.length){
+        throw new ApiError(404,"Channel Not Found")
+    }
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200,channel[0],"Channel Fetched Successfully"))   
+})
+
+
 
 export {registerUser,
     loginUser,
@@ -378,5 +443,6 @@ export {registerUser,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile
 }
